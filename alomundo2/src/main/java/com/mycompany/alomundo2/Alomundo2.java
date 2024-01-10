1f3919e7b0c8c48a476e28af9bf4a69b33d4b705
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2024-01-10 16:32:40 -0300

    java,types of prints, int, string

---
+++ alomundo2/src/main/java/com/mycompany/alomundo2/Alomundo2.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.alomundo2;
+
+
+public class Alomundo2 {
+
+    public static void main(String[] args) {
+        System.out.printf("%s%n","alo mundo");
+    }
+}
---
+++ curso.java
@@ -0,0 +1,16 @@
+public class curso{
+    public static void main(String[] args){
+
+        int num = 10;
+        num = 50;
+        int altura = 170;
+        String nome = "Rodrigo";
+
+        System.out.printf("altura/cm:%d  %nNome:%s", altura, nome);
+
+        System.out.println("\ncurso de java");
+        System.out.print("java is better\n");
+        System.out.printf("%s - %d", "java", 2024);
+        System.out.printf("better language:%s - year:%d", "java", 2024);
+    }
+}
