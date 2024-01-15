64fb7576cd060e82d938291839b37b831fc1f4e0
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2024-01-15 16:18:50 -0300

    5, while, do/while

--- aula5.java
+++ aula5.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
+
 public class aula5 {
     public static void main(String[] args){
+        /* 
+        for(int cont = 0; cont < 5; cont++){
+            System.out.println( cont + "aula");
 
-        for(int cont = 0; cont < 10; cont++){
+        } */
+        
+        /* 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("digite o máximo de vezes");
+        int max = scan.nextInt();
 
-        }
+          int cont = 0;
+
+        while(cont < max) {
+
+        System.out.println( (cont+1) +"- aula");
+        cont ++;
+        } */
+
+        int cont = 0;
+
+        do{
+             System.out.println( (cont+1) +"- aula");
+            cont ++;
+        }while(cont < 0);
+
+        System.out.println("fim de aula");
     }
 }
