69d969a0e9139d3210d026b457d8bcf13cf374a3
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2024-01-19 11:52:23 -0300

    aula12, set,get

--- Jogador.java
+++ Jogador.java
@@ -1,12 +1,34 @@
 public class Jogador{
         public int num = 1000;
         private int numPriv = 0;
+        private int vidas = 0;
+        final int maxVidas = 10;
 
         public Jogador(int num){
         
         // num = num;
         this.num = num;
+        this.vidas = 3;
 
         System.out.printf("o jogador número %d--------", num);
     }
+
+    public int getVidas(){
+        return this.vidas;
+    }
+
+    public void setVidas(int vidas){
+
+        if(vidas < this.maxVidas){
+            this.vidas = maxVidas;
+
+        }else if(vidas < 0){
+            this.vidas = 0;
+            
+        }else{
+            this.vidas = vidas;
+
+        }
+    }
+
 }
--- aula10.java
+++ aula10.java
@@ -6,10 +6,16 @@ public class aula10 {
         Jogador j2 = new Jogador(++num);
         Jogador j3 = new Jogador(++num);
 
-        //j1.numPriv; 
+        j1.setVidas(100000);
+
+        System.out.printf("vidas do jogador: %d", j1.getVidas());
+
+
+        /*j1.numPriv; 
         j1.num = 1000;
         j2.num = 3000;
         System.out.printf("%d", j1.num);
         System.out.printf("%d", j2.num);
+        */
     }
 }
