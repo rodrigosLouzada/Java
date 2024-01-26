96fb576507c06fb43a6223687d2f983c08ffdd2b
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2024-01-26 16:06:25 -0300

    arrays

---
+++ Aula24.java
@@ -0,0 +1,17 @@
+public class Aula24 {
+    public static void main(String[] args){
+
+        int [] num = new int[5];
+        int [] num1 = {10,20,30,40,50};
+
+        for(int i = 0; i < num1.length; i++){
+            System.out.printf("%d-", num1[i]);
+        }
+
+        for(int n : num1){
+            System.out.printf("%d", n);
+        }
+
+    }
+    
+}
