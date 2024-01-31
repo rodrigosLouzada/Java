c767bdbba5e6c7477e191653a7c4ac5964bc2280
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2024-01-31 11:32:54 -0300

    matrix being filled with two 'for' loops

--- Aula32.java
+++ Aula32.java
@@ -8,19 +8,28 @@ public class Aula32 {
 
         int[][] numeros = new int[linha][coluna];
 
-        for(int l = 0; l<linha ; l++){
-            for(int c = 0; c < coluna; c++){
-                numeros[l][c] = new SecureRandom().nextInt(100);
+        gerarDados(numeros,linha,coluna);
+        impDados(numeros, linha, coluna);
+     }
+
+
+     public static void gerarDados(int[][] mtz, int linhas, int colunas){
+         for(int l = 0; l<linhas ; l++){
+            for(int c = 0; c < colunas; c++){
+                mtz[l][c] = new SecureRandom().nextInt(100);
 
             }
         }
-
-        for(int l = 0; l<linha ; l++){
-            for(int c = 0; c < coluna; c++){
-                System.out.printf("%d", numeros[l][c]) ;
-
-            }
-        }
+     }
         
+    
+    public static void impDados(int[][] mtz, int linhas, int colunas){
+    for(int l = 0; l<linhas ; l++){
+            for(int c = 0; c < colunas; c++){
+                System.out.printf("%d - ", mtz[l][c]);
+
+            }
+            System.out.printf("%n");
+        }
     }
 }
