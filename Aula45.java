3af1ea423d0c08109983c6acc2666b6d4f9639f7
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2024-02-09 12:28:13 -0300

    changes bytes from one file to another

--- Aula45.java
+++ Aula45.java
@@ -3,15 +3,16 @@ import java.nio.file.*;
 public class Aula45 {
     public static void main(String[] args){
 
-        String l1 = "C:\Users\natal\OneDrive\Imagens";
         
-
-         Path logoNova = Paths.get("C:\Users\natal\OneDrive\Imagens")
+        Path logoNova = Paths.get("C:/Users/natal/OneDrive/Imagens/escudoNovo.jpg");
+        Path logoAntiga = Paths.get("C:/Users/natal/OneDrive/Imagens/logoANtigo.png");
 
         try{
+            byte[] bytesLogoNovo = Files.readAllBytes(logoNova);
+            Files.write(logoAntiga, bytesLogoNovo);
 
         }catch(Exception e){
-            System.out.println("erro");
+            System.out.println("erro" + e.getMessage());
         }
     }
     
