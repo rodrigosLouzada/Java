161dac07f52b92b8098c9c36e675791c7f20bd05
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2024-02-20 11:56:36 -0300

    lambda, forEach, if, arraylist, consumer

---
+++ Aula51.java
@@ -0,0 +1,40 @@
+import java.util.function.Consumer;
+import java.util.ArrayList;
+
+public class Aula51 {
+    public static void main(String[] args){
+        ArrayList<Integer> valores = new ArrayList<Integer>();
+        ArrayList<Integer> dobro = new ArrayList<Integer>();
+        ArrayList<Integer> par = new ArrayList<Integer>();
+        ArrayList<Integer> impar = new ArrayList<Integer>();
+
+        valores.add(1);
+        valores.add(2);
+        valores.add(3);
+        valores.add(4);
+        valores.add(5);
+
+
+        Consumer<Integer> dobrar = (v) -> {dobro.add(v*2);};
+        valores.forEach(dobrar);
+
+        valores.forEach((v) -> {
+
+            dobro.add(v*2);
+
+            if(v%2 == 0){
+                par.add(v);
+            }else{
+                impar.add(v);
+            }
+        
+        });
+
+        System.out.println(valores);
+        System.out.println(dobro);
+        System.out.println(par);
+        System.out.println(impar);
+
+    }
+
+}
